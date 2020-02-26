540097c8f89a05bf70fc186b5c413e4bd6551b5b
irsyadhusni214 <irsyadaqiila27@gmail>
2020-02-26 10:12:32 +0700

    CRUD

---
+++ resources/views/dosen/index.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.app')
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-10">
+            <div class="card">
+                <div class="card-header">
+                     @if (session('message'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('message') }}
+                        </div>
+                    @endif
+                    Data Dosen
+                    <a href="{{route('dosen.create')}}"
+                        class="float-right">
+                        Tambah Data
+                    </a>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>Nomor</th>
+                                    <th>Nama</th>
+                                    <th>Nipd</th>
+                                    <th>Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @php $no = 1; @endphp
+                                @foreach($dosen as $data)
+                                <tr>
+                                    <td>{{$no++}}</td>
+                                    <td>{{$data->nama}}</td>
+                                    <td>{{$data->nipd}}</td>
+                                    <td>
+                                        <a href="">Lihat</a> |
+                                        <a href="">Ubah</a> |
+                                        <a href="">Hapus</a>
+                                    </td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
